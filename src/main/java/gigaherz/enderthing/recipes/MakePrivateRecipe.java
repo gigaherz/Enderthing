6615de53a097ba7fc2583b49420f924b4377c9d9
gigaherz <gigaherz@gmail.com>
2016-12-24 17:27:56 +0100

    Potential fix for an issue I don't know if happens.

--- src/main/java/gigaherz/enderthing/recipes/MakePrivateRecipe.java
+++ src/main/java/gigaherz/enderthing/recipes/MakePrivateRecipe.java
@@ -34,9 +34,10 @@ public class MakePrivateRecipe extends ShapedOreRecipe
     @Override
     public ItemStack getCraftingResult(InventoryCrafting inv)
     {
-        ItemStack out = getRecipeOutput();
+        ItemStack out = super.getCraftingResult(inv);
 
-        assert out != null;
+        if (out == null || out.stackSize <= 0)
+            return null;
 
         ItemStack itemStack = inv.getStackInSlot(4);
 
@@ -45,8 +46,6 @@ public class MakePrivateRecipe extends ShapedOreRecipe
         NBTTagCompound tag = itemStack.getTagCompound();
         if (tag != null)
             tag = tag.copy();
-        else
-            tag = new NBTTagCompound();
 
         out.setTagCompound(tag);
 
