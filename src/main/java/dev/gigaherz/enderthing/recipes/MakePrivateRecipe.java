b48826aea2805ab9fd3e7b8b84997951d7776b4e
gigaherz <gigaherz@gmail.com>
2024-07-10 00:05:08 +0200

    Update to latest neo

--- src/main/java/dev/gigaherz/enderthing/recipes/MakePrivateRecipe.java
+++ src/main/java/dev/gigaherz/enderthing/recipes/MakePrivateRecipe.java
@@ -4,42 +4,15 @@ import dev.gigaherz.enderthing.Enderthing;
 import dev.gigaherz.enderthing.KeyUtils;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.common.crafting.IShapedRecipe;
+import net.neoforged.neoforge.common.Tags;
 
-public class MakePrivateRecipe extends CustomRecipe implements IShapedRecipe<CraftingInput>
+public class MakePrivateRecipe extends CustomRecipe
 {
     public MakePrivateRecipe(CraftingBookCategory cat)
     {
         super(cat);
-    }
-
-    @Override
-    public boolean matches(CraftingInput inv, Level worldIn)
-    {
-        ItemStack centerSlot = inv.getItem(4);
-        return inv.getItem(0).getCount() == 0
-                && inv.getItem(1).getItem() == Items.GOLD_NUGGET
-                && inv.getItem(2).getCount() == 0
-                && inv.getItem(3).getItem() == Items.GOLD_NUGGET
-                && inv.getItem(5).getItem() == Items.GOLD_NUGGET
-                && inv.getItem(6).getCount() == 0
-                && inv.getItem(7).getItem() == Items.GOLD_NUGGET
-                && inv.getItem(8).getCount() == 0
-                && centerSlot.is(KeyUtils.CAN_MAKE_PRIVATE)
-                && !KeyUtils.isPrivate(centerSlot);
-    }
-
-    @Override
-    public ItemStack assemble(CraftingInput inv, HolderLookup.Provider lookup)
-    {
-        ItemStack output = inv.getItem(4).copy();
-
-        KeyUtils.setPrivate(output, true);
-
-        return output;
     }
 
     @Override
@@ -49,26 +22,36 @@ public class MakePrivateRecipe extends CustomRecipe implements IShapedRecipe<Cra
     }
 
     @Override
+    public boolean matches(CraftingInput inv, Level worldIn)
+    {
+        if (inv.width() != 3 || inv.height() != 3)
+            return false;
+
+        ItemStack centerSlot = inv.getItem(1,1);
+        return inv.getItem(0,0).getCount() == 0
+            && inv.getItem(0,1).is(Tags.Items.NUGGETS_GOLD)
+            && inv.getItem(0,2).getCount() == 0
+            && inv.getItem(1,0).is(Tags.Items.NUGGETS_GOLD)
+            && inv.getItem(1,2).is(Tags.Items.NUGGETS_GOLD)
+            && inv.getItem(2,0).getCount() == 0
+            && inv.getItem(2,1).is(Tags.Items.NUGGETS_GOLD)
+            && inv.getItem(2,2).getCount() == 0
+            && centerSlot.is(KeyUtils.CAN_MAKE_PRIVATE)
+            && !KeyUtils.isPrivate(centerSlot);
+    }
+
+    @Override
+    public ItemStack assemble(CraftingInput inv, HolderLookup.Provider lookup)
+    {
+        if (inv.width() != 3 || inv.height() != 3)
+            return ItemStack.EMPTY;
+
+        return KeyUtils.setPrivate(inv.getItem(1,1).copy(), true);
+    }
+
+    @Override
     public RecipeSerializer<?> getSerializer()
     {
         return Enderthing.MAKE_PRIVATE.get();
-    }
-
-    @Override
-    public RecipeType<?> getType()
-    {
-        return RecipeType.CRAFTING;
-    }
-
-    @Override
-    public int getWidth()
-    {
-        return 3;
-    }
-
-    @Override
-    public int getHeight()
-    {
-        return 3;
     }
 }
